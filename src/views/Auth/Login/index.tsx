b264897ca3630956380fd1de0861c15aee667b21
MRoyhanF <froyhan0@gmail.com>
2024-03-25 14:44:22 +0700

    setup styling

--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./Login.module.css";
+import styles from "./Login.module.scss";
 
 const LoginViews = () => {
-    const {push} = useRouter()
-  
-    const handleLogin = () => {
-      push("/product");
-    };
+  const { push } = useRouter();
 
-    return (    
+  const handleLogin = () => {
+    push("/product");
+  };
+
+  return (
     <div className={styles.login}>
-        <h1>Login Page</h1>
-        <button onClick={() => handleLogin()}>Login</button>
-        <p>
+      <h1 className="text-3xl font-bold">Login Page</h1>
+      <button onClick={() => handleLogin()}>Login</button>
+      <p style={{ color: "red", border: "1px solid red", borderRadius: "10px" }}>
         Belum punya akun ?? Registrasi <Link href={"/auth/register"}>Disini</Link>
-        </p>
+      </p>
     </div>
-    )
-}
+  );
+};
 
 export default LoginViews;
